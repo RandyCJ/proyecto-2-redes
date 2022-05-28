912948a95109665c30683271b5c79965b4883860
tocapb <jeremy.mp1512@gmail.com>
2022-05-28 15:37:53 -0600

    agregando prints

--- api/api.py
+++ api/api.py
@@ -4,24 +4,50 @@ import base64
 app = Flask(__name__)
 
 @app.route('/api/dns_resolver', methods=['POST'])
-def resolve(dns='8.8.8.8'):
+def resolve():
     input_json = request.get_json(force=True)
 
     encoded_data = input_json["data"]
+    print("encoded_data")
+    print(encoded_data)
     decoded_data = base64.b64decode(encoded_data).decode("utf-8") 
-    
+    print("decoded_data")
+    print(decoded_data)
+    print("decoded_data type")
+    print(type(decoded_data))
     bytesToSend = str.encode(decoded_data)
-    serverAddressPort   = (dns, 53)
+    print("bytesToSend")
+    print(bytesToSend)
+    print("bytesToSend type")
+    print(type(bytesToSend))
+    serverAddressPort   = ('8.8.8.8', 53)
+    print("serverAddressPort")
+    print(serverAddressPort)
     bufferSize          = 1024
+    print("bufferSize")
+    print(bufferSize)
 
     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    print("UDPClientSocket")
+    print(UDPClientSocket)
     UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    print("UDPClientSocket despues del sendto")
+    print(UDPClientSocket)
+    msgFromServer, _ = UDPClientSocket.recvfrom(bufferSize)
+    print("msgFromServer")
+    print(msgFromServer)
+    msgFromServer = msgFromServer[0]
+    print("msgFromServer despues de sacar el indice 0")
+    print(msgFromServer)
 
-    encoded_answer = base64.b64encode(msgFromServer).decode("utf-8") 
+    encoded_answer = base64.b64encode(msgFromServer).decode("utf-8")
+    print("encoded_answer")
+    print(encoded_answer)
     dictToReturn = {'answer': encoded_answer}
+    print("dictToReturn")
+    print(dictToReturn)
 
     return jsonify(dictToReturn)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=443)
+    app.run(debug=True, host='172.19.0.3', port=443)
