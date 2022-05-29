0140d065a1ad8329a2def33d198d6c647b486d16
tocapb <jeremy.mp1512@gmail.com>
2022-05-29 16:39:05 -0600

    Agregando api funcional

--- api/api.py
+++ api/api.py
@@ -5,42 +5,33 @@ app = Flask(__name__)
 
 @app.route('/api/dns_resolver', methods=['POST'])
 def resolve():
-    input_json = request.get_json(force=True)
+    """
+    Description: Decodes the data from base64
+        sends the information to a remote dns
+        encodes the response and returns it.
+    Input: None
+    Output: A dictionary in json format
+    """
+    try:
+        input_json = request.get_json(force=True)
 
-    encoded_data = input_json["data"]
-    print("encoded_data")
-    print(encoded_data)
-    decoded_data = base64.b64decode(encoded_data)
-    print("decoded_data")
-    print(decoded_data)
-    print("decoded_data type")
-    print(type(decoded_data))
-    serverAddressPort   = ('8.8.8.8', 53)
-    print("serverAddressPort")
-    print(serverAddressPort)
-    bufferSize          = 1024
-    print("bufferSize")
-    print(bufferSize)
+        encoded_data = input_json["data"]
+        remote_dns = input_json["dns"]
+        port = input_json["port"]
 
-    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    print("UDPClientSocket")
-    print(UDPClientSocket)
-    UDPClientSocket.connect(serverAddressPort)
-    print("UDPClientSocket despues del connect")
-    print(UDPClientSocket)
-    UDPClientSocket.sendall(decoded_data)
-    print("UDPClientSocket despues del sendall")
-    print(UDPClientSocket)
-    msgFromServer = UDPClientSocket.recv(bufferSize)
-    print("msgFromServer")
-    print(msgFromServer)
+        decoded_data = base64.b64decode(encoded_data)
+        serverAddressPort = (remote_dns, port)
+        bufferSize = len(decoded_data)
 
-    encoded_answer = base64.b64encode(msgFromServer)
-    print("encoded_answer")
-    print(encoded_answer)
-    dictToReturn = {'answer': encoded_answer}
-    print("dictToReturn")
-    print(dictToReturn)
+        UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        UDPClientSocket.connect(serverAddressPort)
+        UDPClientSocket.sendall(decoded_data)
+        msgFromServer = UDPClientSocket.recv(bufferSize)
+
+        encoded_answer = base64.b64encode(msgFromServer)
+        dictToReturn = {'answer': encoded_answer.decode()}
+    except:
+        dictToReturn = {'answer': 'Wrong data sent'}
 
     return jsonify(dictToReturn)
 
